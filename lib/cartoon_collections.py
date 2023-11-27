92bdd1e4b165ea3690485a3448b33e996061bd9e
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-27 13:17:58 -0500

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i in range(len(dwarves)):
+        print(f"{i+1}. {dwarves[i]}")
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteers):
+    return [planeteer.title() + "!" for planeteer in planeteers]
 
-def find_the_cheese():
-    pass
+
+def long_planeteer_calls(planeteers):
+    for call in planeteers:
+        if len(call) > 4:
+            return True
+    return False
+
+
+def find_the_cheese(strings):
+    for string in strings:
+        if string == "cheddar" or string == "camembert" or string == "gouda":
+            return string
